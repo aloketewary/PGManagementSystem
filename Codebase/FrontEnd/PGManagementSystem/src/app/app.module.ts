ce349303efd3101280d023790e625aa65c1b2589
Aloke Tewary <aloketewary@gmail.com>
2018-01-07 12:38:29 +0530

    Add Login Components

--- Codebase/FrontEnd/PGManagementSystem/src/app/app.module.ts
+++ Codebase/FrontEnd/PGManagementSystem/src/app/app.module.ts
@@ -1,19 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { MatToolbarModule } from '@angular/material';
+import { MatCardModule } from '@angular/material';
 // Import owncomponents here
 
 
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/components/login/login.component';
+import { appRoutes } from './app.route';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Codebase/FrontEnd/PGManagementSystem/src/app/app.route.ts
@@ -0,0 +1,6 @@
+import { LoginComponent } from "./login/components/login/login.component";
+import { RouterModule, Routes } from '@angular/router';
+
+export const appRoutes: Routes = [
+    {path : '' , component : LoginComponent}
+  ];
